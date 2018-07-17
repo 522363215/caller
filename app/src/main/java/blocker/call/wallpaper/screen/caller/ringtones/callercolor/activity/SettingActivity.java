0e6aeb2805b8ec7259a41581f81a8274e5a5df4e
chenrong <https://github.com/sunnychenr/GoogleLocation.git>
2018-07-17 15:42:30 +0800

    update

--- app/src/main/java/blocker/call/wallpaper/screen/caller/ringtones/callercolor/activity/SettingActivity.java
+++ app/src/main/java/blocker/call/wallpaper/screen/caller/ringtones/callercolor/activity/SettingActivity.java
@@ -2,7 +2,11 @@ package blocker.call.wallpaper.screen.caller.ringtones.callercolor.activity;
 
 import android.os.Bundle;
 
+import com.md.block.core.BlockManager;
+
 import blocker.call.wallpaper.screen.caller.ringtones.callercolor.R;
+import blocker.call.wallpaper.screen.caller.ringtones.callercolor.helper.PreferenceHelper;
+import blocker.call.wallpaper.screen.caller.ringtones.callercolor.view.SwitchButton;
 
 /**
  * Created by ChenR on 2018/7/14.
@@ -10,10 +14,29 @@ import blocker.call.wallpaper.screen.caller.ringtones.callercolor.R;
 
 public class SettingActivity extends BaseActivity {
 
+    private SwitchButton sbBlock;
+    private SwitchButton sbCallerId;
+    private SwitchButton sbMessage;
+
+    private boolean enableBlock, enableCallerId, enableMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        initView();
+    }
+
+    private void initView() {
+        sbBlock = findViewById(R.id.sb_block);
+        sbCallerId = findViewById(R.id.sb_caller_id);
+        sbMessage = findViewById(R.id.sb_message);
+
+        enableBlock = BlockManager.getInstance().getBlockSwitchState();
+        enableCallerId = PreferenceHelper.getBoolean(PreferenceHelper.PREF_KEY_ENABLE_SHOW_CALL_AFTER, false);
+
+
     }
 
 
